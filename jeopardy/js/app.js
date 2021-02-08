eed32ae1cefffd4e59e18d5f19a3b8e16c622ac7
Eric Oeur <ericeoeur@gmail.com>
2021-02-07 22:35:07 -0600

    started app.js and building questions and answers. click event on each price category now functioning

--- jeopardy/js/app.js
+++ jeopardy/js/app.js
@@ -1,9 +1,256 @@
-console.log("test");
+const gameCategories = {
+    category1: [{
+        categoryName: 'The Simpsons'
+      }, {
+        costQuestionAnswer: [{
+          cost: 200,
+          question: "Whats lisas dog name",
+          answer: "cat"
+        }],
+      },
+      {
+        costQuestionAnswer: [{
+          cost: 400,
+          question: "Whats lisas cat name",
+          answer: "weee"
+        }],
+      },
+      {
+        costQuestionAnswer: [{
+          cost: 600,
+          question: "Whats lisas werer name",
+          answer: "ass"
+        }],
+      },
+      {
+        costQuestionAnswer: [{
+          cost: 800,
+          question: "Whats lisas ewewewe name",
+          answer: "qaaaaa"
+        }],
+      },
+      {
+        costQuestionAnswer: [{
+          cost: 1000,
+          question: "Whats lisas la name",
+          answer: "weeenus"
+        }],
+      },
+    ],
+    
+    //end of Category 1 
+
+category2: [{
+  categoryName: 'The Simpsons'
+}, {
+  costQuestionAnswer: [{
+    cost: 200,
+    question: "Whats lisas dog name",
+    answer: "cat"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 400,
+    question: "Whats lisas cat name",
+    answer: "weee"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 600,
+    question: "Whats lisas werer name",
+    answer: "ass"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 800,
+    question: "Whats lisas ewewewe name",
+    answer: "qaaaaa"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 1000,
+    question: "Whats lisas la name",
+    answer: "weeenus"
+  }],
+},
+], //end of Category 2 
+
+category3: [{
+  categoryName: 'The Simpsons'
+}, {
+  costQuestionAnswer: [{
+    cost: 200,
+    question: "Whats lisas dog name",
+    answer: "cat"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 400,
+    question: "Whats lisas cat name",
+    answer: "weee"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 600,
+    question: "Whats lisas werer name",
+    answer: "ass"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 800,
+    question: "Whats lisas ewewewe name",
+    answer: "qaaaaa"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 1000,
+    question: "Whats lisas la name",
+    answer: "weeenus"
+  }],
+},
+], //end of Category 3 
+
+category4: [{
+  categoryName: 'The Simpsons'
+}, {
+  costQuestionAnswer: [{
+    cost: 200,
+    question: "Whats lisas dog name",
+    answer: "cat"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 400,
+    question: "Whats lisas cat name",
+    answer: "weee"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 600,
+    question: "Whats lisas werer name",
+    answer: "ass"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 800,
+    question: "Whats lisas ewewewe name",
+    answer: "qaaaaa"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 1000,
+    question: "Whats lisas la name",
+    answer: "weeenus"
+  }],
+},
+], //end of Category 4 
+
+category5: [{
+  categoryName: 'The Simpsons'
+}, {
+  costQuestionAnswer: [{
+    cost: 200,
+    question: "Whats lisas dog name",
+    answer: "cat"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 400,
+    question: "Whats lisas cat name",
+    answer: "weee"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 600,
+    question: "Whats lisas werer name",
+    answer: "ass"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 800,
+    question: "Whats lisas ewewewe name",
+    answer: "qaaaaa"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 1000,
+    question: "Whats lisas la name",
+    answer: "weeenus"
+  }],
+},
+], //end of Category 5
+
+category6: [{
+  categoryName: 'The Simpsons'
+}, {
+  costQuestionAnswer: [{
+    cost: 200,
+    question: "Whats lisas dog name",
+    answer: "cat"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 400,
+    question: "Whats lisas cat name",
+    answer: "weee"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 600,
+    question: "Whats lisas werer name",
+    answer: "ass"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 800,
+    question: "Whats lisas ewewewe name",
+    answer: "qaaaaa"
+  }],
+},
+{
+  costQuestionAnswer: [{
+    cost: 1000,
+    question: "Whats lisas la name",
+    answer: "weeenus"
+  }],
+},
+], //end of Category 6 
+
+}; //end of gamecategories array
+console.log(gameCategories);
 
 
-$(() => {
 
-  
+// $(() => {
+
+// let $answer = $('.cost');
+
+// console.log($answer);
+
+// $answer.on('click', (e) => {
+//   console.log($(e.currentTarget).children().text()); //the text of the number 
+//   console.log($(e.currentTarget).parent().parent());
+// });
 
 
-});
+
+// });
