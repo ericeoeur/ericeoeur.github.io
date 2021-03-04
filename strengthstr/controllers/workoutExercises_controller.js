488c3eb8d5a42ad5442e505fef367d78b7b351d9
Eric Oeur <ericeoeur@gmail.com>
2021-03-03 21:06:52 -0600

    generate workout functioning but need to add formula

--- strengthstr/controllers/workoutExercises_controller.js
+++ strengthstr/controllers/workoutExercises_controller.js
@@ -9,7 +9,7 @@ const Workout = express.Router();
 
 // add shit in here to take in the data from oneExercise and show them 
 
-//SEED
+// ADD CUSTOM WORKOUT 
 Workout.get('/seed', (req, res) =>{
   console.log(req.session.currentUser._id); // you can see user data in here!
   WorkoutExercises.create(
@@ -20,11 +20,98 @@ Workout.get('/seed', (req, res) =>{
       }
     ],
     (error, data) => {
-      console.log("An workoutExercise was created");
+      console.log("An custom workoutExercise was created");
       res.redirect('/workoutExercises');
     }
   )
 })
+
+
+// GENERATE WORKOUT
+Workout.get('/generateWorkout', (req, res) =>{
+  console.log(req.session.currentUser); // you can see user data in here!
+ 
+
+
+let generatedWorkout = new WorkoutExercises({
+  exercises:[],
+  user: req.session.currentUser
+}); 
+
+// generatedWorkout.save((err, generatedWorkout) => {
+//   if (err) {
+//     console.log(err);
+//     res.status(400).send("There is an error while adding a generated workout"); 
+//   } else {
+//     console.log("Successully added generated workout!")
+//   }
+// })
+
+
+User.findById(req.session.currentUser._id).populate('oneRepMaxes').
+  exec(function (err, userData) {
+    if (err) return handleError(err);
+    console.log("These are the 1 rep maxes for the user"); 
+
+
+    for (let i =0; i <userData.oneRepMaxes.length; i++){
+    console.log(userData.oneRepMaxes[i].liftName);
+    console.log(userData.oneRepMaxes[i].weight);
+  }
+
+
+let generatedSquat = OneExercise({
+  liftName: userData.oneRepMaxes[0].liftName, 
+  weight: userData.oneRepMaxes[0].weight,
+  sets: '3',
+  reps: '5',
+  completed: null
+}); 
+
+let generatedBench = OneExercise({
+  liftName: userData.oneRepMaxes[1].liftName, 
+  weight: userData.oneRepMaxes[1].weight,
+  sets: '3',
+  reps: '5',
+  completed: null
+}); 
+
+let generatedPress = OneExercise({
+  liftName: userData.oneRepMaxes[2].liftName, 
+  weight: userData.oneRepMaxes[2].weight,
+  sets: '3',
+  reps: '5',
+  completed: null
+}); 
+
+let generatedDeadlift = OneExercise({
+  liftName: userData.oneRepMaxes[3].liftName, 
+  weight: userData.oneRepMaxes[3].weight,
+  sets: '1',
+  reps: '5',
+  completed: null
+}); 
+
+
+Promise.all([
+  generatedSquat.save(),
+  generatedBench.save(), 
+  generatedPress.save(),
+  generatedDeadlift.save()
+]).then(function(generatedExercises){
+  generatedWorkout.exercises = generatedExercises;
+  generatedWorkout.save(); 
+  res.redirect('/workoutExercises'); 
+}).catch(function(err) {
+  console.log("Error saving generated workout");
+  console.log(err); 
+}); 
+
+})
+})
+
+
+
 
 // SHOW
 Workout.get('/:id', (req, res) => {
